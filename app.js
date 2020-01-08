70b56115732ce8cce510bda711bd462fc0018933
bcarlso1 <bcarlso1@gmail.com>
2020-01-07 20:16:53 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* ********
+Populate page
+************* */
+
 const parentDiv = document.getElementById('employees');
 const url = 'https://randomuser.me/api/?results=12';
 
@@ -11,10 +15,37 @@ fetch(url)
         <h4>${employee.name.first} ${employee.name.last}</h4>
           <p>${employee.email}</p>
           <p>${employee.location.city}</p>
-        <div>
+        <div class="more-detail">
+        <hr>
+        <p>${employee.cell}</p>
+        <p>${employee.location.street.name} ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>   
+        <p>Birthday: ${employee.dob.date.slice(5, 7)}/${employee.dob.date.slice(8,10)}/${employee.dob.date.substr(0,4)} 
+        </div>
+        </div>
         `;
       employeeDiv.classList.add("employee");
       parentDiv.appendChild(employeeDiv);
   }));
   
+/* ***********
+MODAL
+*********** */
+window.onload = function() {
+  document.getElementById('modal').style.display = "none"
+} 
 
+
+
+  document.getElementById('employees').addEventListener("click", (e) => {
+    document.getElementById('modal-text').innerHTML = event.target.innerHTML;
+    if (document.getElementById('modal').style.display == "none") {
+     document.getElementById('modal').style.display = "block";
+     
+   }
+   });
+
+document.getElementById('modal-x').addEventListener("click", (e) => {
+  if (document.getElementById('modal').style.display == "block") {
+    document.getElementById('modal').style.display = "none";
+  }  
+}); 
